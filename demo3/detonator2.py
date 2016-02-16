25b676aa1d4f79958c9c14c66ab4e758c54ea29c
David Whale <david@thinkingbinaries.com>
2016-02-16 22:25:35 +0000

    Final tweaks before workshop on mac

--- demo3/detonator2.py
+++ demo3/detonator2.py
@@ -1,9 +1,11 @@
 import mcpi.minecraft as minecraft
 import mcpi.block as block
 import time
-import countdown
+import countdown_mac as countdown
 
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO  # Raspberry Pi
+import anyio.GPIO as GPIO # Arduino
+
 BUTTON = 4
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON, GPIO.IN)
@@ -14,15 +16,15 @@ mc.postToChat("PRESS THE BUTTON!")
 def bomb(x, y, z):
 	mc.setBlock(x+1, y, z+1, block.TNT.id)
 
-	#for a in range(5, 0, -1):
-	#	mc.postToChat(str(a))
-	#	time.sleep(1)
-	countdown.count()
+	for a in range(5, 0, -1):
+		mc.postToChat(str(a))
+		time.sleep(1)
+	####countdown.count()
 
 	mc.postToChat("BANG!")
 	mc.setBlocks(x-10, y-5, z-10, x+10, y+10, z+10, block.AIR.id)
-	#mc.player.setTilePos(x, y+40, z)
-	countdown.bang()
+	####mc.player.setTilePos(x, y+40, z)
+	####countdown.bang()
 
 try: 
 	while True:
